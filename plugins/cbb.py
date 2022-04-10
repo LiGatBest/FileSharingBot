1617f9675491dd8f929ac7aa5c6008543a5b450d
LiGatBest <103337202+LiGatBest@users.noreply.github.com>
2022-04-10 08:18:46 +0800

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,27 @@
+# read all my data u will find the answer
+# share to all but make adjustment here n there
+# t.me/UnlimitedTrusted & t.me/TekanSiniLah
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import Bot
+from config import CHANNEL, GROUP, OWNER
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://t.me/IWasNotABot'>Klik Disini</a>\n • Owner Repo: @IWasNotABot</b>\n",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
+            ),
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except BaseException:
+            pass
